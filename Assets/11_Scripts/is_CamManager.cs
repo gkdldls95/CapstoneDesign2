b31eaf54d70f4499c266c7477a389a5d1659f0dc
LeeYin <gkdldls95@naver.com>
2021-09-23 14:00:47 +0900

    FPS

---
+++ Assets/11_Scripts/is_CamManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class is_CamManager : MonoBehaviour
+{
+    //회전 속도 변수
+    public float rotSpeed = 1500f;
+    public Transform target;
+
+    //회전 값 변수
+    float mx = 0;
+    float my = 0;
+
+    void Update()
+    {
+        //사용자의 마우스 입력을 받아 물체를 회전시키고 싶다.
+        //마우스 입력을 받는다.
+
+        float mouse_X = Input.GetAxis("Mouse X");
+        float mouse_Y = Input.GetAxis("Mouse Y");
+
+        //회전 값 변수에 마우스 입력 값만큼 미리 누적시킨다.
+        mx += mouse_X * rotSpeed * Time.deltaTime;
+        my += mouse_Y * rotSpeed * Time.deltaTime;
+
+        //마우스 상하 이동 회전 변수(my)의 값을 -90도~90도 사이로 제한한다.
+        my = Mathf.Clamp(my, -40f, 50f);
+
+        transform.eulerAngles = new Vector3(-my, mx, 0);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        transform.position = target.position;
+    }
+}
---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public InputField NickNameInput;
+    public GameObject DisconnectPanel;
+    //public GameObject RespawnPanel;
+
+    void Awake()
+    {
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 6 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        DisconnectPanel.SetActive(false);
+    }
+
+    void Update() { if (Input.GetKeyDown(KeyCode.Escape) && PhotonNetwork.IsConnected) PhotonNetwork.Disconnect(); }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        DisconnectPanel.SetActive(true);
+        //RespawnPanel.SetActive(false);
+    }
+}
